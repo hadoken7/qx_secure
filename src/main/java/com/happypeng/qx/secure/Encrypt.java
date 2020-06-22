6ff99d31bee7d9e9a6a1aa2a2c4076894c0bb372
Hadoken <wangpeng2015@gmail.com>
2020-06-22 14:40:41 +0800

    Rc4 + base64

--- src/main/java/com/happypeng/qx/secure/Encrypt.java
+++ src/main/java/com/happypeng/qx/secure/Encrypt.java
@@ -10,11 +10,13 @@ public interface Encrypt {
 
     /**
      * rc4加密
+     *
      * @param content
      * @param key
      * @return
      */
     default String encrypt(String content, String key) {
-        return Base64.getEncoder().encodeToString(Rc4Util.rc4(content,key).getBytes());
+        byte[] bytes = Rc4Util.rc4Base(content.getBytes(), key);
+        return Base64.getEncoder().encodeToString(bytes);
     }
 }
